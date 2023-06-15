e91bb074bb967d6ce477371156a7dc5de354852a
Rajesh_laptop <berarajesh40@gmail.com>
2023-06-15 15:52:32 +0530

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./navbar.scss";
+import { Icon } from "@iconify/react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-wrapper">
+        <div className="logo">
+          <h2>E-COMMERCE</h2>
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search.." />
+          <button>
+            <Icon icon="iconamoon:search" width="20" height="20" />
+          </button>
+        </div>
+        <div className="cart">
+          <Icon icon="material-symbols:shopping-cart" width="20" height="20" />
+          <span>Cart</span>
+          <div className="batch">1</div>
+        </div>
+        <div className="login">Login</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
