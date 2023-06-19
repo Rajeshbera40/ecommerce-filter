994880811b6fcbcadf1f426d8e186429c6e9b329
RajeshBera94 <berabhabesh@gmail.com>
2023-06-19 16:13:04 +0530

    add product card

---
+++ src/components/productcard/ProductCard.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./productcard.scss";
+const ProductCard = () => {
+    return (
+        <div className="productcard">
+            <div className="card">
+                <div className="product_image">
+                    <img
+                        src="https://rukminim1.flixcart.com/image/312/312/xif0q/mobile/9/z/o/-original-imaghuf9guqmb65z.jpeg?q=70"
+                        alt=""
+                    />
+                </div>
+                <div className="product_desc">
+                    <div className="desc_about">
+                        <h4 className="title">
+                            realme C33 2023 (Aqua Blue, 128 GB)
+                        </h4>
+                        <div className="p_rating">
+                            <span>4.4</span>
+                            <span>13,111 Ratings </span>
+                            <span> 406 Reviews</span>
+                        </div>
+
+                        <div className="p_details">
+                            <ul>
+                                <li>
+                                    4 GB RAM | 128 GB ROM | Expandable Upto 1 TB
+                                </li>
+                                <li>
+                                    4 GB RAM | 128 GB ROM | Expandable Upto 1 TB
+                                </li>
+                                <li>
+                                    4 GB RAM | 128 GB ROM | Expandable Upto 1 TB
+                                </li>
+                                <li>
+                                    4 GB RAM | 128 GB ROM | Expandable Upto 1 TB
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="desc_price">
+                        <h3 className="P_price">₹10,499</h3>
+                        <div className="p_off">
+                            <span>₹13,999</span>
+                            <span>25% off</span>
+                        </div>
+                        <div className="dlivery">
+                            <span>Free delivery by</span>
+                            <span>Today</span>
+                        </div>
+                        <div className="deal">Back to Campus Deal</div>
+                        <div className="exchange">
+                            <span>Upto </span>
+                            <span>₹9,400</span>
+                            <span> Off on Exchange</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Leftfilterbar from "../components/leftfilterbar/Leftfilterbar";
+import ProductCard from "../components/productcard/ProductCard";
 const Home = () => {
-  return (
-    <div className="home">
-        <Navbar />
-      <div className="home-wrapper">
-        <div className="left">
-          <Leftfilterbar />
+    return (
+        <div className="home">
+            <Navbar />
+            <div className="home-wrapper">
+                <div className="left">
+                    <Leftfilterbar />
+                </div>
+                <div className="right">
+                    <ProductCard />
+                </div>
+            </div>
         </div>
-        <div className="right"></div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
