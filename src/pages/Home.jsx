699703612ce76217d7ac78e9368300c6ef23273a
Rajesh_laptop <berarajesh40@gmail.com>
2023-06-19 12:36:43 +0530

    navbar leftsidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Navbar from "../components/navbar/Navbar"
-import Leftfilterbar from '../components/leftfilterbar/Leftfilterbar'
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import Leftfilterbar from "../components/leftfilterbar/Leftfilterbar";
 const Home = () => {
   return (
     <div className="home">
-      <Navbar/>
+        <Navbar />
       <div className="home-wrapper">
         <div className="left">
-          <Leftfilterbar/>
+          <Leftfilterbar />
         </div>
         <div className="right"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
