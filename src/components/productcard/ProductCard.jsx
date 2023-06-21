cca7e25c42662cc21ad800060f4fc9a6ade95bc3
RajeshBera94 <berabhabesh@gmail.com>
2023-06-21 13:32:42 +0530

    update product

--- src/components/productcard/ProductCard.jsx
+++ src/components/productcard/ProductCard.jsx
@@ -1,66 +1,91 @@
 import React from "react";
 import "./productcard.scss";
-const ProductCard = () => {
+const ProductCard = ({ Data }) => {
     return (
         <div className="productcard">
-            <div className="card">
-                <div className="product_image">
-                    <img
-                        src="https://rukminim1.flixcart.com/image/312/312/xif0q/mobile/9/z/o/-original-imaghuf9guqmb65z.jpeg?q=70"
-                        alt=""
-                    />
-                </div>
-                <div className="product_desc">
-                    <div className="desc_about">
-                        <h4 className="title">
-                            realme C33 2023 (Aqua Blue, 128 GB)
-                        </h4>
-                        <div className="p_rating">
-                            <span className="num_rating">4.4 &#9733;</span>
-                            <span className="user_rating">
-                                13,111 Ratings &nbsp;&
-                            </span>
-
-                            <span className="reviews">&nbsp; 406 Reviews</span>
+            {Data &&
+                Data.map((data, i) => (
+                    <div className="card" key={i}>
+                        <div className="product_image">
+                            {data.src && <img src={data.src} alt="" />}
                         </div>
+                        <div className="product_desc">
+                            <div className="desc_about">
+                                <h4 className="title">{data.name}</h4>
+                                <div className="p_rating">
+                                    {data.point_rating && (
+                                        <span className="num_rating">
+                                            {data.point_rating} &#9733;
+                                        </span>
+                                    )}
+                                    {data.rating && (
+                                        <span className="user_rating">
+                                            {data.rating} &nbsp;&
+                                        </span>
+                                    )}
+                                    {data.reviews && (
+                                        <span className="reviews">
+                                            &nbsp; {data.reviews}
+                                        </span>
+                                    )}
+                                </div>
 
-                        <div className="p_details">
-                            <ul>
-                                <li>
-                                    4 GB RAM | 128 GB ROM | Expandable Upto 1 TB
-                                </li>
-                                <li>
-                                    4 GB RAM | 128 GB ROM | Expandable Upto 1 TB
-                                </li>
-                                <li>
-                                    4 GB RAM | 128 GB ROM | Expandable Upto 1 TB
-                                </li>
-                                <li>
-                                    4 GB RAM | 128 GB ROM | Expandable Upto 1 TB
-                                </li>
-                            </ul>
+                                <div className="p_details">
+                                    <ul>
+                                        {data.ram && <li>{data.ram}</li>}
+                                        {data.screen && <li>{data.screen}</li>}
+                                        {data.camera && <li>{data.camera}</li>}
+                                        {data.bettery && (
+                                            <li>{data.bettery}</li>
+                                        )}
+                                        {data.processor && (
+                                            <li>{data.processor}</li>
+                                        )}
+                                        {data.warranty && (
+                                            <li>{data.warranty}</li>
+                                        )}
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="desc_price">
+                                {data.price && (
+                                    <span className="p_price">
+                                        {data.price}
+                                    </span>
+                                )}
+
+                                <div className="p_off">
+                                    {data.mrp && (
+                                        <span className="p_rate">
+                                            {data.mrp}
+                                        </span>
+                                    )}
+                                    {data.discount && (
+                                        <span className="p_discount">
+                                            {data.discount}
+                                        </span>
+                                    )}
+                                </div>
+                                <div className="dlivery">
+                                    <span>
+                                        Free delivery by <strong>Today</strong>
+                                    </span>
+                                </div>
+                                <div className="deal">Back to Campus Deal</div>
+                                <div className="exchange">
+                                    <span>Upto </span>
+                                    {data.exchange && (
+                                        <span className="exchange_value">
+                                            {data.exchange}
+                                        </span>
+                                    )}
+
+                                    <span> Off on Exchange</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="desc_price">
-                        <span className="p_price">₹10,499</span>
-                        <div className="p_off">
-                            <span className="p_rate">₹13,999</span>
-                            <span className="p_discount">25% off</span>
-                        </div>
-                        <div className="dlivery">
-                            <span>
-                                Free delivery by <strong>Today</strong>
-                            </span>
-                        </div>
-                        <div className="deal">Back to Campus Deal</div>
-                        <div className="exchange">
-                            <span>Upto </span>
-                            <span className="exchange_value">₹9,400</span>
-                            <span> Off on Exchange</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                ))}
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Leftfilterbar from "../components/leftfilterbar/Leftfilterbar";
 import ProductCard from "../components/productcard/ProductCard";
+import Data from "../data.json";
+
 const Home = () => {
+    console.log(Data);
     return (
         <div className="home">
             <Navbar />
@@ -11,8 +14,7 @@ const Home = () => {
                     <Leftfilterbar />
                 </div>
                 <div className="right">
-                    <ProductCard />
-                    <ProductCard />
+                    <ProductCard Data={Data} />
                 </div>
             </div>
         </div>
